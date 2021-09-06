65d0588462a9797277fbd835f41250db24df1d7a
Emmanuel289 <emmirald@gmail.com>
2021-09-06 05:07:27 -0600

    Deleted unused import statement

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
-import playground
 from django.contrib import admin
 from django.urls import path, include
 
